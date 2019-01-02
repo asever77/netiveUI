414b466488510eef74a88761b6e957d654990175
asever77 <johyunmin77@gmail.com>
2019-01-03 01:11:15 +0900

    d

--- resources/js/ui.base.js
+++ resources/js/ui.base.js
@@ -38,7 +38,7 @@
 
     script_file += '<script src="/resources/js/ui.global.js"></script>';
     script_file += '<script src="/resources/js/ui.plugins.js"></script>';
-     script_file += '<script src="/resources/js/ui.common.js"></script>';
+    script_file += '<script src="/resources/js/ui.common.js"></script>';
     script_file += '<script src="/resources/js/ui.page.js"></script>';
 
     
