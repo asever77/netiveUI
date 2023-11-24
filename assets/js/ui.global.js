e42295c7be29ac7dbbb1080b8aebd796103d8469
asever77 <johyunmin77@gmail.com>
2023-11-24 16:08:39 +0900

    d

--- assets/js/ui.global.js
+++ assets/js/ui.global.js
@@ -190,7 +190,9 @@
                 switch (type) {
                     case 'HTML' :
                         console.log('src', src);
-                        fetch(src).then(response => response.text()).then(data => {
+                        fetch(src)
+                        .then(response => response.text())
+                        .then(data => {
                             if (insert) {
                                 selector.insertAdjacentHTML('afterbegin', data);
                             } else {
