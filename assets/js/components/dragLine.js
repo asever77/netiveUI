6af06db75cb47d15be02216def294a94b299ddd7
asever77 <johyunmin77@gmail.com>
2024-05-05 01:20:34 +0900

    수정

--- assets/js/components/dragLine.js
+++ assets/js/components/dragLine.js
@@ -7,6 +7,7 @@ export default class DragLine {
             '[data-line-object], [data-line-target]'
         );
         this.objects = this.wrap.querySelectorAll(`[data-line-object]`);
+        this.targets = this.wrap.querySelectorAll(`[data-line-target]`);
         
         this.type = this.wrap.dataset.lineType
             ? this.wrap.dataset.lineType
@@ -69,33 +70,34 @@ export default class DragLine {
         });
         resizeObserver.observe(this.wrap);
 
-        let click_info = {
-            currentTarget: null,
+        let firstTouch = {
+            state: false,
+            item: null,
+            line: null,
             x: null,
             y: null,
-            name: null,
-            isObject: null
+            is_object : null,
         };
+        let moving = false;
         const actStart = e => {
-            console.log('actStart----', e.type);
-            
-            //클릭 이벤트가 없는 경우 line생성
-            if (this.wrap.dataset.event !== 'click-start' && this.wrap.dataset.event !== 'click-end') {
-                console.log('line생성');
+            console.log(moving, e.type, firstTouch.state);
 
-                this.wrap.querySelector('svg').insertAdjacentHTML(
-                    'beforeend',
-                    `<line x1="0" x2="0" y1="0" y2="0" data-state="ing"></line>`
-                );
-            } 
-            //클릭 이벤트가 있고, 마우스다운 이벤트라면 click 이벤트가 뒤에 일어나기 때문에 중지.
-
-            console.log('actStart----', this.wrap.dataset.event === 'click-start' && (e.type === 'mousedown' || e.type === 'touchstart'));
-
-            if (this.wrap.dataset.event === 'click-start' && (e.type === 'mousedown' || e.type === 'touchstart')) {
-                return false;
+            if (this.isTouch) {
+                if (!firstTouch.state) {
+                    this.wrap.querySelector('svg').insertAdjacentHTML(
+                        'beforeend',
+                        `<line x1="0" x2="0" y1="0" y2="0" data-state="ing"></line>`
+                    );
+                }
+            } else {
+                if (!moving) {
+                    this.wrap.querySelector('svg').insertAdjacentHTML(
+                        'beforeend',
+                        `<line x1="0" x2="0" y1="0" y2="0" data-state="ing"></line>`
+                    );
+                }
             }
-            
+
             this.wrap_t = this.wrap.getBoundingClientRect().top;
             this.wrap_l = this.wrap.getBoundingClientRect().left;
             this.wrap_w = this.wrap.offsetWidth;
@@ -104,9 +106,9 @@ export default class DragLine {
             let el_line = this.svg.querySelector('line[data-state="ing"]');
             const el_item = e.currentTarget;
             const rect_item = el_item.getBoundingClientRect();
-            const is_object = el_item.dataset.lineObject ? true : false;
+            let is_object = el_item.dataset.lineObject ? true : false;
             const data_name = el_item.dataset.name;
-            const value = is_object
+            let value = is_object
                 ? el_item.dataset.lineObject
                 : el_item.dataset.lineTarget;
             const item_w = el_item.offsetWidth / 2;
@@ -117,31 +119,28 @@ export default class DragLine {
             let _y;
             let actMove;
             let actEnd;
-    
+
+            if (moving) {
+                el_item.dataset.active = "";
+            } else {
+                el_item.dataset.active = "true";
+            }
+            
+            _x = e.clientX ? e.clientX : e.targetTouches[0].clientX;
+            _y = e.clientY ? e.clientY : e.targetTouches[0].clientY;
+
             actEnd = () => {
-                console.log('actEnd----', this.wrap.dataset.event === 'click-start' && (e.type === 'mousedown' || e.type === 'touchstart'));
-
-                //클릭완료이벤트에 마우스다운, 터치스타트인경우 이벤트 취소 및 종료 : 클릭이벤트만 적용하기 위해
-                if (this.wrap.dataset.event === 'click-start' && (e.type === 'mousedown' || e.type === 'touchstart')) {
-                    this.doc.removeEventListener('mousemove', actMove);
-                    this.doc.removeEventListener('touchmove', actMove);
-                    this.doc.removeEventListener('mouseup', actEnd);
-                    this.doc.removeEventListener('touchend', actEnd);
-                    return false;
-                }
-
+                console.log('actEnd----', e.type);
                 //클릭완료이벤트에 클릭이벤트인경우 클릭완료 설정
-                if (this.wrap.dataset.event === 'click-start' && e.type === 'click') {
-                    click_info.currentTarget.dataset.active = "";
-                    this.wrap.dataset.event = "click-end";
-                }
-
                 const v_x = _x - this.wrap_l;
                 const v_y = _y - this.wrap_t;
                 let is_complete = false;
                 let is_answer = false;
 
+                moving = false;
                 el_line.dataset.state = 'complete';
+                el_item.dataset.active = "";
+                firstTouch.state = false;
 
                 for (let item of this.items) {
                     // item.dataset.state = '';
@@ -159,13 +158,14 @@ export default class DragLine {
                     const if_y = v_y >= i_y - item_h && v_y <= i_y - item_h + item_h * 2;
                     let is_selected = false;
                     let connect_array;
+               
                     //1:1규칙용
                     //특정영역안에 있고, 같은 분류가 아니며, 완료되지않은 아이템
                     const if_compete =
                         this.type === 'single'
                             ? if_x && if_y && is_object !== _is_object && !_is_complete
                             : if_x && if_y && is_object !== _is_object;
-
+                        
                     //1:n인 경우 이미 연결된 아이템 제외
                     if (this.type === 'multiple' && item.dataset.connect) {
                         connect_array = item.dataset.connect.split(',');
@@ -178,6 +178,8 @@ export default class DragLine {
                         }
                     }
 
+                    console.log(is_object, _is_object);
+
                     //연결성공
                     if (if_compete && !is_selected) {
                         //연결된 아이템 완료상태
@@ -185,6 +187,7 @@ export default class DragLine {
                         item.dataset.complete = true;
                         //완료갯수
                         this.complete_n = this.complete_n + 1;
+
                         //아이템 연결된 정보
                         if (!item.dataset.connect) {
                             item.dataset.connect = el_item.dataset.name;
@@ -198,6 +201,7 @@ export default class DragLine {
                         el_line.setAttribute('y2', _rect_item.top + item_h - this.wrap_t);
 
                         //정오답적용
+                        console.log('value',value);
                         const v1 = value.split(',');
                         const v2 = _value.split(',');
 
@@ -223,7 +227,9 @@ export default class DragLine {
                                 is_answer = false;
                             }
                         }
+
                         is_complete = true;
+
                         if (el_item.dataset.lineObject) {
                             this.answer_last.push({
                                 ['key_' + el_item.dataset.name]: el_item.dataset.lineObject,
@@ -238,9 +244,9 @@ export default class DragLine {
                         break;
                     }
                 }
+                
                 //연결 실패인 경우 라인 삭제
                 if (!is_complete) el_line.remove();
-
                 this.doc.removeEventListener('touchmove', actMove);
                 this.doc.removeEventListener('touchend', actEnd);
                 this.doc.removeEventListener('mousemove', actMove);
@@ -248,89 +254,81 @@ export default class DragLine {
 
                 if (this.callback) {
                     this.callback({
-              /*전체정답갯수*/ answer_all_sum: this.answer_len,
-              /*현재정답갯수*/ answer_current_sum: this.answer_n,
-              /*선택한정답  */ answer_current: value,
-              /*정오답상태  */ answer_state: is_answer,
-              /*히스토리    */ answer_last: this.answer_last,
+                    /*전체정답갯수*/ answer_all_sum: this.answer_len,
+                    /*현재정답갯수*/ answer_current_sum: this.answer_n,
+                    /*선택한정답  */ answer_current: value,
+                    /*정오답상태  */ answer_state: is_answer,
+                    /*히스토리    */ answer_last: this.answer_last,
                     });
                 }
                 if (this.complete_n === this.n) this.completeCallback();
+                
+                console.log('----------------------------------------------')
             };
             actMove = e => {
                 _x = e.clientX ? e.clientX : e.targetTouches[0].clientX;
                 _y = e.clientY ? e.clientY : e.targetTouches[0].clientY;
                 el_line.setAttribute('x2', _x - this.wrap_l);
                 el_line.setAttribute('y2', _y - this.wrap_t);
+                if (this.isTouch) {
+                    this.doc.addEventListener('touchend', actEnd);
+                } else {
+                    this.doc.addEventListener('mouseup', actEnd);
+                }
+                moving = true;
+                console.log('move', moving);
             };
 
-            //클릭시작이 아니라면 이벤트 실행
-            if (this.wrap.dataset.event !== 'click-start') {
-                this.doc.addEventListener('mousemove', actMove);
-                this.doc.addEventListener('mouseup', actEnd);
-                this.doc.addEventListener('touchmove', actMove);
-                this.doc.addEventListener('touchend', actEnd);
-                
-                //클릭완료가 아니라면 실행 
-                if (this.wrap.dataset.event !== 'click-end') {
+            if (this.isTouch) {
+                if (!firstTouch.state) {
+                    console.log('click start');
+                    this.doc.removeEventListener('touchmove', actMove);
+                    this.doc.addEventListener('touchmove', actMove);
+                    
+                    firstTouch.state = true;
+                    firstTouch.item = el_item;
+                    firstTouch.line = el_line;
+                    firstTouch.x = x_value;
+                    firstTouch.y = y_value;
+                    firstTouch.is_object = is_object;
+                   
                     el_line.setAttribute('x1', x_value);
                     el_line.setAttribute('y1', y_value);
                     el_line.setAttribute('x2', x_value);
                     el_line.setAttribute('y2', y_value);
+
+                    console.log('aaaaaaaaaaaaa')
+                } else {
+                    console.log('click end');
+                    // this.doc.removeEventListener('touchmove', actMove);
+                    firstTouch.state = false;
+                    firstTouch.item.dataset.active = "";
+                    el_item.dataset.active = "";
+                    is_object = firstTouch.is_object;
+                    value = is_object ? firstTouch.item.dataset.lineObject : firstTouch.item.dataset.lineTarget;
+                    _x = e.clientX ? e.clientX : e.targetTouches[0].clientX;
+                    _y = e.clientY ? e.clientY : e.targetTouches[0].clientY;
                     
-                    //클릭이벤트라면 클릭시작 설정 및 클릭정보 저장
-                    if (e.type === 'click' ) {
-                        this.wrap.dataset.event = "click-start";
-                        el_item.dataset.active = "true";
-                        click_info = {
-                            line: el_line,
-                            currentTarget: el_item,
-                            x: x_value,
-                            y: y_value,
-                            name: data_name,
-                            isObject: is_object
-                        }
-                    } 
-                } 
-                //클릭완료인 경우 이벤트 취소
-                else {
-                    this.wrap.dataset.event= "";
-                    this.doc.removeEventListener('mousemove', actMove);
-                    this.doc.removeEventListener('mouseup', actEnd);
+                    console.log('bbbbbbbbbbbbbbb')
+
                     this.doc.removeEventListener('touchmove', actMove);
                     this.doc.removeEventListener('touchend', actEnd);
+                    actEnd();
+                    
                 }
-            } 
-            //클릭시작이 이미 존재하는 경우 무브,앤드 이벤트 취소
-            else if (this.wrap.dataset.event === 'click-start') {
-                this.doc.removeEventListener('mousemove', actMove);
-                this.doc.removeEventListener('mouseup', actEnd);
-                this.doc.removeEventListener('touchmove', actMove);
-                this.doc.removeEventListener('touchend', actEnd);
-
-                if (e.type === 'click') {
-                    click_info.currentTarget.dataset.active = "";
-                    el_line = click_info.line;
-                    
-                    if (click_info.isObject !== is_object) {
-                        el_line.setAttribute('x1', click_info.x);
-                        el_line.setAttribute('y1', click_info.y);
-                        actEnd();
-                    } 
-                } else if (e.type === 'touchstart') {
-                    click_info.currentTarget.dataset.active = "";
-                    el_line = click_info.line;
-                    
-                    if (click_info.isObject !== is_object) {
-                        el_line.setAttribute('x2', x_value);
-                        el_line.setAttribute('y2', y_value);
-                    } 
-                } 
-            } 
-            
+            } else {
+                if (!moving) {
+                    this.doc.addEventListener('mousemove', actMove);
+    
+                    el_line.setAttribute('x1', x_value);
+                    el_line.setAttribute('y1', y_value);
+                    el_line.setAttribute('x2', x_value);
+                    el_line.setAttribute('y2', y_value);
+                }
+            }
         };
 
-        for (let item of this.items) {
+        for (const item of this.items) {
             if (this.isTouch) {
                 item.addEventListener('touchstart', actStart, {
                     passive: true,
@@ -338,7 +336,7 @@ export default class DragLine {
             } else {
                 item.addEventListener('mousedown', actStart);
             }
-            item.addEventListener('click', actStart);
+            // item.addEventListener('click', actStart);
         }
     }
     completeCallback() {
